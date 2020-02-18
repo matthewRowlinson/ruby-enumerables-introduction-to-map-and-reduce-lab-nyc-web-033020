01ce67dc2cd9c4663fe8589762979ff3ee5d79b9
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-18 19:27:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# My Code here....
+# # My Code here....
+# ### `map`-like
+
+# - `map_to_negativize(source_array)`
+# - `map_to_no_change(source_array)`
+# - `map_to_double(source_array)`
+# - `map_to_square(source_array)`
+
+# Remember, all `map` methods return a new `Array`.
+
+# ### `reduce`-like
+
+# - `reduce_to_total(source_array, starting_point)`
+# - `reduce_to_all_true(source_array)`
+# - `reduce_to_any_true(source_array)`
+
+# Remember, all `reduce` methods return a _value_.
