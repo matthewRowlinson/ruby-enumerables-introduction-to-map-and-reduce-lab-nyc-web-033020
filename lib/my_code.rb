e499856c56770dfa80439f8f5c80b123ebdc7e66
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-18 19:36:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -72,5 +72,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
+  result = false   
   i = 0 
   while i < source_array.length 
+    if source_array[i] == true
+      return true
+    end
+  end
+  result 
+end
